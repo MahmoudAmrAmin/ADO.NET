3a2d14f96844594f2cbdbb808285e63c1f6a60da
Mahmoud Amr <mahmoudamr500@gmail>
2024-09-16 08:21:19 +0300

    add files of project

---
+++ ExecuteSqlRaw/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Data;
+
+
+
+namespace ExecuteSqlRaw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
+            var connectionToDB = new SqlConnection(connectionString.GetSection("constr").Value);
+
+            var selectStatment = "select * from Wallets";
+
+            SqlCommand command = new SqlCommand(selectStatment, connectionToDB);    
+
+            // this line tell complier that command not storded procedure 
+            command.CommandType = CommandType.Text;
+            
+            // open connection to DB 
+            connectionToDB.Open();
+            
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            Wallet wallet;
+
+            while (reader.Read())
+            {
+                wallet = new Wallet
+                {
+                    Id = reader.GetInt32("Id"),
+                    Holder = reader.GetString("Holder"),
+                    Balance = reader.GetDecimal("Balance")
+                };
+
+                Console.WriteLine(wallet);
+            }
+
+            connectionToDB.Close();
+
+        }
+    }
+}
---
+++ stringConnection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Configuration; 
+namespace stringConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var connectionString = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
+
+            // test configurations
+
+            Console.WriteLine(connectionString.GetSection("constr").Value);
+
+        }
+
+
+
+    }
+}
