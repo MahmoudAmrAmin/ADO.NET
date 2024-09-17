fbb7d69b398dfe65dfef39bdfbc388e5e413097c
Mahmoud Amr <mahmoudamr500@gmail>
2024-09-17 03:29:13 +0300

    add Execute insert scaler

---
+++ ExecuteInsertExecuteScaler/Program.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace ExecuteInsertExecuteScaler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = new ConfigurationBuilder()
+                 .AddJsonFile("appSettings.json")
+                 .Build();
+
+
+
+            SqlConnection? connectionToDB = new SqlConnection(connectionString.GetSection("constr").Value);
+
+
+            // read data to insert it in DB 
+
+            Wallet NewWallet = new Wallet()
+            {
+                Holder = "mansour",
+                Balance = 160000m
+            };
+            
+
+
+
+
+            var insertStatment = "INSERT INTO WALLETS (Holder, Balance) VALUES " +
+                $"(@Holder, @Balance);"+ 
+                $"SELECT CAST(scope_identity() AS int)";
+
+            /*"INSERT INTO WALLETS (Holder, Balance) VALUES " +
+                            $"(@Holder, @Balance);" +
+                            $"SELECT CAST(scope_identity() AS int)";*/
+
+
+
+            // first Parameter
+
+            SqlParameter holderParameter = new SqlParameter()
+            {
+                ParameterName = "@Holder",
+                SqlDbType = SqlDbType.VarChar,
+                Direction = ParameterDirection.Input,
+                Value = NewWallet.Holder
+            };
+
+            SqlParameter balanccParamter = new SqlParameter()
+            {
+                ParameterName = "@Balance",
+                SqlDbType = SqlDbType.Decimal,
+                Direction = ParameterDirection.Input,
+                Value = NewWallet.Balance
+
+            };
+
+
+            //It is used to represent a SQL statement or stored procedure that will be executed against a SQL Server database.
+            SqlCommand command = new SqlCommand(insertStatment, connectionToDB);
+
+            command.Parameters.Add(holderParameter);
+            command.Parameters.Add(balanccParamter);
+
+            // this line tell complier that command not storded procedure
+            command.CommandType = CommandType.Text;
+
+            // open connection to DB 
+            connectionToDB.Open();
+
+
+            NewWallet.Id = (int)command.ExecuteScalar();
+
+            Console.WriteLine($"wallet {NewWallet} added successully");
+
+
+
+
+
+            connectionToDB.Close();
+
+        }
+    }
+}
